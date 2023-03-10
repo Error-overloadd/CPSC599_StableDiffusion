e6f2c4bd5d4017e60633486b7519fe5bedb39885
Error-overloadd <liudongjie1119@gmail.com>
2023-03-10 16:29:25 -0700

    createBasicfile

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// The image tracking
+import 'mind-ar/dist/mindar-image.prod.js';
+import 'mind-ar/dist/mindar-image-aframe.prod.js';
+//The face tracking
+import 'mind-ar/dist/mindar-face.prod.js';
+import 'mind-ar/dist/mindar-face-aframe.prod.js';
+
+import 'aframe';
---
+++ src/node_modules/mind-ar/dist/mindar-image.prod.js
@@ -0,0 +1,13 @@
+import { C as o, a as r } from "./controller-939e6d85.js";
+import { U as i } from "./ui-56ede774.js";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
